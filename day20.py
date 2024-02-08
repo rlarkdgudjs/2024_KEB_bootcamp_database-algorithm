d7722c612358563187aba924ec223a4032ca0a56
rlarkdgudjs <rlarkdgudjs@gmail.com>
2024-02-08 11:25:21 +0900

    v0.5

--- day20.py
+++ day20.py
@@ -1,10 +1,7 @@
 import mymath
-import time
+
 
 if __name__ == "__main__":
     n = int(input("INPUT n : "))
     r = int(input("INPUT r : "))
-    start = time.time()
     print(f"{n}C{r} = {mymath.nCr(n,r)}")
-    end = time.time()
-    print(end-start)
--- mymath.py
+++ mymath.py
@@ -1,3 +1,12 @@
+import time
+def timer(func):
+    def wrap(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"time elapsed : {end-start}")
+        return result
+    return wrap
 # def factorial(num) -> int :
 #     """
 #     factorial by repetition
@@ -20,6 +29,7 @@ def factorial(num) -> int :
     else:
         return num * factorial(num-1)
 
+@timer
 def nCr(n,r) -> int :
     """
     조합 함수
